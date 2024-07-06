b4c6c948bf2a1688e628b27fda8966f842014582
TheadventureofZelda <ledacloc5@gmail.com>
2024-07-06 15:41:40 +0700

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("download").addEventListener("click", function() {    
-    this.textContent = "Downloading...";
-    setTimeout(function() {
-        document.getElementById("download").textContent = "Download";
-    }, 2000);
-});
